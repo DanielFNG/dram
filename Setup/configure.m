8d877e9be214e6f7caf24bb767ae078880ca7112
Daniel Gordon <daniel.gordon@ed.ac.uk>
2018-10-14 18:12:40 +0100

    Rename configuration file

--- Setup/configure.m
+++ Setup/configure.m
@@ -1,9 +1,9 @@
-function configureExopt()
+function configure()
 % Adds the appropriate Gait Analysis Toolbox source directories to the
 % Matlab path. 
 
 % Modify the Matlab path to include the source folder.
-addpath(genpath(['..' filesep 'Source'));
+addpath(genpath(['..' filesep 'Source']));
 
 % Include any additional libraries. 
 addpath(genpath(['..' filesep 'External' filesep 'multiWaitbar']));
